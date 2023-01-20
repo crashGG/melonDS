b761c0377f97e48c40fe1da992ab250e3464a2c0
Brooksytech <macebrooks@gmail.com>
2023-01-20 14:06:55 -0800

    Update main.h

--- src/frontend/qt_sdl/main.h
+++ src/frontend/qt_sdl/main.h
@@ -409,6 +409,8 @@ public:
     QActionGroup* grpScreenLayout;
     QAction* actScreenLayout[4];
     QAction* actScreenSwap;
+    QAction* actSaveStateHK;
+    QAction* actLoadStateHK;
     QActionGroup* grpScreenSizing;
     QAction* actScreenSizing[6];
     QAction* actIntegerScaling;
