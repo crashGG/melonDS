4283b2dda06c4a90658eba3767ec98c737cb0a8d
Brooksytech <macebrooks@gmail.com>
2023-01-20 16:45:56 -0800

    Update main.cpp

--- src/frontend/qt_sdl/main.cpp
+++ src/frontend/qt_sdl/main.cpp
@@ -426,7 +426,9 @@ EmuThread::EmuThread(QObject* parent) : QThread(parent)
     connect(this, SIGNAL(screenLayoutChange()), mainWindow->panelWidget, SLOT(onScreenLayoutChanged()));
     connect(this, SIGNAL(windowFullscreenToggle()), mainWindow, SLOT(onFullscreenToggled()));
     connect(this, SIGNAL(swapScreensToggle()), mainWindow->actScreenSwap, SLOT(trigger()));
-
+    connect(this, SIGNAL(hkSaveState()), mainWindow->actSaveState[1], SLOT(onSaveState()));
+    connect(this, SIGNAL(hkLoadState()), mainWindow->actLoadState[1], SLOT(onLoadState()));
+	
     static_cast<ScreenPanelGL*>(mainWindow->panel)->transferLayout(this);
 }
 
@@ -597,8 +599,8 @@ void EmuThread::run()
 
         if (Input::HotkeyPressed(HK_SwapScreens)) emit swapScreensToggle();
 		
-		if (Input::HotkeyPressed(HK_SaveState)) break;
-		if (Input::HotkeyPressed(HK_LoadState)) break;
+        if (Input::HotkeyPressed(HK_SaveState)) emit hkSaveState();
+        if (Input::HotkeyPressed(HK_LoadState)) emit hkLoadState();
 
         if (Input::HotkeyPressed(HK_SolarSensorDecrease))
         {
