a0b6ce0aef3574b5ed26532585055f02843bf903
Brooksytech <macebrooks@gmail.com>
2023-01-20 16:16:39 -0800

    Update main.cpp

--- src/frontend/qt_sdl/main.cpp
+++ src/frontend/qt_sdl/main.cpp
@@ -1635,8 +1635,16 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
         {
             QMenu* submenu = menu->addMenu("Save state");
 
-            actSaveState[1] = submenu->addAction("Save State");
-            actSaveState[1]->setShortcut(Input::HotkeyPressed(HK_SaveState));
+            for (int i = 1; i < 9; i++)
+            {
+                actSaveState[i] = submenu->addAction(QString("%1").arg(i));
+                actSaveState[i]->setShortcut(QKeySequence(Qt::ShiftModifier | (Qt::Key_F1+i-1)));
+                actSaveState[i]->setData(QVariant(i));
+                connect(actSaveState[i], &QAction::triggered, this, &MainWindow::onSaveState);
+            }
+
+            actSaveState[1] = submenu->addAction("File...");
+            actSaveState[1]->setShortcut((Input::HotkeyPressed(HK_SaveState)));
             actSaveState[1]->setData(QVariant(1));
             connect(actSaveState[1], &QAction::triggered, this, &MainWindow::onSaveState);
         }
@@ -1652,7 +1660,7 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
             }
 
             actLoadState[0] = submenu->addAction("File...");
-            actLoadState[0]->setShortcut(QKeySequence(Qt::Key_F9));
+            actLoadState[0]->setShortcut(QKeySequence((Qt::Key_F9) || (Input::HotkeyPressed(HK_LoadState))));
             actLoadState[0]->setData(QVariant(0));
             connect(actLoadState[0], &QAction::triggered, this, &MainWindow::onLoadState);
         }
